dcd2ac22ce484120367abd15a7276f850c9b8d0f
Luis Preza <preza.luis@gmail.com>
2020-05-06 17:18:52 -0500

    five minutes tutorial

--- myscript.ts
+++ myscript.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+class Student {
+  fullname: string;
+  constructor(public firstname, public middleinitial, public lastname){
+    this.fullname = firstname + ' ' + middleinitial + ' ' + lastname;
+  }
+}
+
+interface Person {
+  firstname: string;
+  lastname: string;
+}
+
+function greeter(person: Person){
+  return `Hello, ${person.firstname} ${person.lastname}`;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.textContent = greeter(user);
